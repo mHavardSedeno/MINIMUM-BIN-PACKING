e62b54ca762f32d065b9faab8fc8b828de15d0c0
mHavardSedeno <mathilde.havard---sedeno@etu.univ-nantes/fr>
2020-11-09 11:26:57 +0100

    Create mainMinBP.jl

---
+++ mainMinBP.jl
@@ -0,0 +1,9 @@
+#Minimum Bin-Packing
+#Projet de complexité - M. FERTIN
+#Augustin HERVE & Mathilde HAVARD-SEDENO
+#M1 Informatique
+
+fname = "C:/Users/Félix/Documents/GitHub/MINIMUM-BIN-PACKING/instance1.txt"
+#loadMBP(fname)
+#inputMBP()
+generateMBP(2,2)
