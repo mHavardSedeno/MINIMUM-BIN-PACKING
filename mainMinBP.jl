df74a114ca9b5398a7862cf0ddbc4aaad6ef5faa
mHavardSedeno <mathilde.havard---sedeno@etu.univ-nantes/fr>
2020-12-01 15:05:18 +0100

    Update mainMinBP.jl

--- mainMinBP.jl
+++ mainMinBP.jl
@@ -7,6 +7,21 @@ include("parser.jl")
 include("binPacking.jl")
 
 #Choix de l'instance
+println("__ CHOIX DE L'INSTANCE __")
+println("\n 1- Depuis un fichier; \n 2- Au clavier; \n 3- Génération aléatoire. \n")
+println("Entrez le code du type d'instance souhaitée : ")
+type = readline()
+
+while (type != 1) && (type != 2) && (type != 3)
+    print("non")
+end
+
+if type == 1
+
+elseif type == 2
+
+else
+
 #Chargement
 fname = "C:/Users/Félix/Documents/GitHub/MINIMUM-BIN-PACKING/instance1.txt"
 n, obj = loadMBP(fname)
